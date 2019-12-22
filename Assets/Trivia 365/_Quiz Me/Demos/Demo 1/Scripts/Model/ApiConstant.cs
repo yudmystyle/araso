09944029b72ae44528034ebab9d93364bcda1658
yudha aditya <yudha_aditya03@yahoo.com>
2019-12-22 23:07:33 +0700

    add http

--- Assets/Trivia 365/_Quiz Me/Demos/Demo 1/Scripts/Model/ApiConstant.cs
+++ Assets/Trivia 365/_Quiz Me/Demos/Demo 1/Scripts/Model/ApiConstant.cs
@@ -1,5 +1,5 @@
 ﻿static class ApiConstant
 {
-    public const string SERVER = "10.209.138.123:8000";
+    public const string SERVER = "http://10.209.138.123:8000";
     //public const string SERVER = "http://157.245.60.103:9009";
 }
