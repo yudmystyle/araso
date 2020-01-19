93ae0ea13471943d3c4f498ed5ca25b92002f680
yudha aditya <yudha_aditya03@yahoo.com>
2020-01-19 20:23:11 +0700

    Auto stash before merge of "duel" and "origin/duel"

--- Assets/Trivia 365/_Quiz Me/Demos/Demo 1/Scripts/Model/ApiConstant.cs
+++ Assets/Trivia 365/_Quiz Me/Demos/Demo 1/Scripts/Model/ApiConstant.cs
@@ -1,5 +1,5 @@
 ﻿static class ApiConstant
 {
-    public const string SERVER = "localhost:8000";
+    public const string SERVER = "http://192.168.1.7:8000";
     //public const string SERVER = "http://157.245.60.103:9009";
 }
